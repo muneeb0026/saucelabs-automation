c4d2435ac7fbfd86369dce5f4806943467a7fcb4
Muneeb Riaz <munebahmed2020@mail.com>
2026-02-17 22:19:02 +0500

    Updated config file

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
 
   e2e: {
     specPattern: "e2e/**/*.cy.js",
+    fixturesFolder: "fixtures",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
